3ea265d2babacb0a2fc48afc7d84692df090a858
Exolan <97833724+Exolan@users.noreply.github.com>
2022-06-01 17:43:02 +0300

    Add files via upload

--- FinalProject/buttonDelete.js
+++ FinalProject/buttonDelete.js
@@ -3,6 +3,14 @@ const tr = document.querySelectorAll(".table-body-row");
 
 for (let i = 0; i < buttonsDelete.length; i++) {
   buttonsDelete[i].addEventListener("click", () => {
-    tr[i].remove();
+    const modalDelete = document.querySelector(".modal-delete");
+    const deleteClient = modalDelete.querySelector(".delete-client");
+    let clientArray = JSON.parse(localStorage.getItem("clients"));
+
+    modalDelete.style.display = "flex";
+
+    deleteClient.addEventListener("click", () => {
+      tr[i].remove();
+    });
   });
 }
--- FinalProject/modalChange.js
+++ FinalProject/modalChange.js
@@ -1,19 +1,31 @@
-const modalChange = document.querySelector(".modal-change");
-const closeButtonModal = modalChange.querySelector(".modal-close");
-const buttons = document.querySelectorAll(".change");
-for (let i = 0; i < buttons.length; i++) {
-  buttons[i].addEventListener("click", () => {
-    modalChange.style.display = "flex";
+const buttonsChange = document.querySelectorAll(".change");
 
-    closeButtonModal.addEventListener("click", () => {
-      modalChange.style.display = "none";
+for (let i = 0; i < buttonsChange.length; i++) {
+  buttonsChange[i].addEventListener("click", () => {
+    const modal = document.querySelector(".modal-change");
+    const closeButton = modal.querySelector(".modal-close");
+    const inputes = modal
+      .querySelector(".modal-inputes")
+      .querySelectorAll("input");
+    const clientArray = JSON.parse(localStorage.getItem("clients"));
+    const fio = clientArray[i].fio.split(" ");
+    console.log(fio);
+
+    modal.style.display = "flex";
+
+    for (let j = 0; j < inputes.length; j++) {
+      inputes[j].value = fio[j];
+    }
+
+    closeButton.addEventListener("click", () => {
+      modal.style.display = "none";
     });
 
-    modalChange.addEventListener("click", (e) => {
+    modal.addEventListener("click", (e) => {
       const isModal = e.target.closest(".modal-conteiner");
       const divCont = e.target.closest(".block");
       if (!isModal && !divCont) {
-        modalChange.style.display = "none";
+        modal.style.display = "none";
       }
     });
   });
